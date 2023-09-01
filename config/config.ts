609703295240240be786faf7c07d426cba73b0d8
tianma <2223486623@qq.com>
2023-09-01 15:34:16 +0800

    --

--- config/config.ts
+++ config/config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: 'gh-framework',
+  title: 'Gh Framework',
   layout: {
     locale: true,
     ...defaultSettings,
--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -15,7 +15,7 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'gh-framework',
+  title: 'Gh Framework',
   pwa: true,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
